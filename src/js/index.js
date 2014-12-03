097594a7fe85b010193e9925ec671f1db39cddaa
David Thomas Grübl <me@davidgruebl.com>
2014-12-03 18:43:46 +0100

    feat: include image in multiple resolutions in zip file

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,9 @@ var $ = require('jquery')
 var resize = require('./lib/resize')
 var zip = new (require('jszip'))
 
-var thisisuglyashell = ''
+var tiny = ''
+var medium = ''
+var full = ''
 
 $(function() {
   var $body = $('body')
@@ -24,19 +26,25 @@ $(function() {
   })
   .load(function() {
     var preview = new Image
-    preview.src = thisisuglyashell = resize(img, 250)
+    preview.src = tiny = resize(img, 250)
+    medium = resize(img, 1024)
+    full = resize(img)
     $preview
       .text('')
       .prepend(preview)
     $(this).remove()
+    $purchase.removeAttr('disabled')
   })
   .appendTo($body)
+
 
   $purchase.click(function() {
     while(prompt('gimme da code') !== 'fhsgmbh') {}
 
     var folder = zip.folder('images')
-    folder.file('preview.jpg', thisisuglyashell.split('base64,')[1], {base64: true})
+    folder.file('tiny.jpg', tiny.split('base64,')[1], {base64: true})
+    .file('medium.jpg', medium.split('base64,')[1], {base64: true})
+    .file('full.jpg', full.split('base64,')[1], {base64: true})
 
     location.href="data:application/zip;base64," + zip.generate({type:"base64"});
   })
